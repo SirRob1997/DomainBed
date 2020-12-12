7fd3f383e768faad2e940a44b2bebf7fa0436798
Robin Schmidt <robin.schmidt.97@web.de>
2020-12-12 18:59:21 +0100

    fixes

--- domainbed/hparams_registry.py
+++ domainbed/hparams_registry.py
@@ -49,7 +49,7 @@ def _hparams(algorithm, dataset, random_seed):
         _hparam('rsc_b_drop_factor', 1/3, lambda r: r.uniform(0, 0.5))
 
     elif algorithm == "ProDrop":
-        _hparam('num_prototypes_per_class', 40, lambda r: 40)
+        _hparam('num_prototypes_per_class', 10, lambda r: 10)
         _hparam('prototype_width', 1, lambda r: 1)
         _hparam('prototype_height', 1, lambda r: 1)
         _hparam('additional_losses', False, lambda r: False)
@@ -61,7 +61,7 @@ def _hparams(algorithm, dataset, random_seed):
         _hparam('end_to_end', True, lambda r: True)
         _hparam('pp_lr', 3e-3, lambda r: 3e-3)
         _hparam('pp_weight_decay', 1e-3, lambda r: 1e-3)
-        _hparam('warmup_steps', 25, lambda r: 25)
+        _hparam('warmup_steps', 100, lambda r: 100)
 
     elif algorithm == "SagNet":
         _hparam('sag_w_adv', 0.1, lambda r: 10**r.uniform(-2, 1))
